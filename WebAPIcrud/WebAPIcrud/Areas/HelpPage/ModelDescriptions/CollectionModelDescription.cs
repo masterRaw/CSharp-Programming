b111afc31f32d71a8a0397b6e6657be38af79b8a
masterRaw <50144522+masterRaw@users.noreply.github.com>
2021-06-22 14:17:07 +0530

    Delete WebAPIcrud directory

--- WebAPIcrud/WebAPIcrud/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace WebAPIcrud.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
