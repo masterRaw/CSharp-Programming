b058e145debe860bb5b52ee5c960be2c16a6dadc
Abhinav Rawat <Abhinav.Rawat@hanu.com>
2021-06-25 15:26:17 +0530

    Add Project Files.

---
+++ Caesar Cipher/Caesar Cipher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string to encrypted: ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Enter the string to encrypted: ");
+            int x  = Convert.ToInt32(Console.ReadLine());
+
+            calc(str,x);
+        }
+
+        static void calc(string str, int x)
+        {
+            char[] strlist = str.ToCharArray();
+            char[] cipher = new char[str.Length];
+
+            for (int i = 0; i < strlist.Length; i++)
+            {
+                if (Char.IsLetter(strlist[i]))
+                {
+                    cipher[i]= (char)((int)(strlist[i]+ x));
+                }
+            }
+
+            Console.WriteLine(cipher);
+        }
+    }
+}
