3477f2676f4a317fb2807f6d4cd1f33e5e22d92b
Abhinav Rawat <Abhinav.Rawat@hanu.com>
2021-06-25 15:01:26 +0530

    Add Project Files.

---
+++ Number Search/Number Search/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Number_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ask();
+        }
+
+        static void ask()
+        {
+            Console.WriteLine("Enter the String: ");
+            string str = Console.ReadLine();
+            calc(str);
+        }
+
+        static void calc(string str)
+        {
+            double sum = 0;
+            double letter = 0;
+           
+            char[] strlist = str.ToCharArray();
+
+            for (int i = 0; i < strlist.Length; i++)
+            {
+                if (char.IsLetter(strlist[i]))
+                { 
+                    letter++;
+                }
+                if (char.IsNumber(strlist[i]))
+                {
+                    sum += Convert.ToDouble(strlist[i].ToString());
+                }
+            }
+
+            double total1 = sum / letter;
+            var total = (int)Math.Round(total1);
+            Console.WriteLine(total+"\n");
+
+            ask();
+
+        }
+    }
+}
